7bc420a4da3b05819b57737b37d1fc8bae6f729b
Ramiro Gómez <code@ramiro.org>
2017-05-03 20:50:02 +0200

    Append CoderStats link rather than cloning posibly non-existing link item. Don't listen on DOMSubtreeModified event

--- coderstats/data/coderstats.js
+++ coderstats/data/coderstats.js
@@ -1,5 +1,4 @@
-var link = document.getElementsByClassName('octicon-link')[0],
-    path = document.location.pathname,
+var path = document.location.pathname,
     details,
     login,
     url;
@@ -11,7 +10,6 @@ if (m = path.match(/^\/([\w-]+)\??.*?/)) {
         details = document.getElementsByClassName('vcard-details');
         if (details.length > 0) {
             addLink();
-            document.addEventListener('DOMSubtreeModified', navClick, false);
         }
     }
 }
@@ -26,24 +24,16 @@ function addLink() {
     li.setAttribute('class', 'vcard-detail pt-1');
     li.setAttribute('itemprop', 'url');
 
-    var a = document.createElement('a');
+    let span = document.createElement('span');
+    span.setAttribute('class', 'octicon');
+    span.setAttribute('style', 'margin-top:-2px;');
+    span.textContent = "📊";
+    li.appendChild(span)
+
+    let a = document.createElement('a');
     a.setAttribute('href', url);
     a.textContent = "CoderStats('" + login + "')";
 
-    var svg = link.cloneNode();
-    svg.appendChild(link.childNodes[0].cloneNode())
-
-    li.appendChild(svg);
     li.appendChild(a);
     details[0].appendChild(li);
-}
-
-
-// https://github.com/KyroChi/GitHub-Profile-Fluency/blob/Google-Chrome/chrome/content.js
-function navClick() {
-    document.removeEventListener('DOMSubtreeModified', navClick);
-    setTimeout(function() {
-        addLink();
-        document.addEventListener('DOMSubtreeModified', navClick, false);
-    }, 500);
 }
